075e0ba0bda911c3e527e0bdea4cf1db23b8e2d4
Gustavo Silvera <grs5382@gmail.com>
2022-11-04 22:30:26 -0400

    refactoring the game class

--- illusion.py
+++ illusion.py
@@ -3,8 +3,9 @@ import random
 
 
 class Circle:
-    def __init__(self, center, color, radius):
-        self.center = center
+    def __init__(self, x: float, y: float, color: tuple, radius: float):
+        self.x = x
+        self.y = y
         self.color = color
         self.radius = radius
 
@@ -12,18 +13,27 @@ class Circle:
 class Game:
     idx2rgb = {0: (255, 0, 0), 1: (0, 255, 0), 2: (0, 0, 255)}
     rgb2idx = {value: key for key, value in idx2rgb.items()}
+    circle_color = (181, 142, 53)  # the actual (background colour background)
 
     def __init__(self, h, w, line_ht=2):
         self.SCREEN_HEIGHT = h
         self.SCREEN_WIDTH = w
 
-        self.init_game()
         self.line_ht = line_ht
-
-    def init_game(self):
         self.screen = pygame.display.set_mode([self.SCREEN_WIDTH, self.SCREEN_HEIGHT])
         self.line_ht = 2
-        self.colors = [random.randint(a=0, b=2) for _ in range(9)]
+        self.circles = [
+            Circle(
+                x=self.SCREEN_WIDTH * (i % 3 + 1) / 4,
+                y=self.SCREEN_HEIGHT * (i // 3 + 1) / 4,
+                radius=min(self.SCREEN_HEIGHT, self.SCREEN_WIDTH) * 0.1,
+                color=Game.idx2rgb[random.randint(a=0, b=2)],
+            )
+            for i in range(9)
+        ]
+
+        # inputs
+        self.clicked = False
 
     def draw_background(self):
         self.screen.fill((0, 0, 0))  # background
@@ -36,32 +46,45 @@ class Game:
             self.screen.blit(line, (0, y))
             y += self.line_ht
 
-    def draw_circles(self, clicked: bool):
-        circles = []
-        for i in range(9):
-            x = self.SCREEN_WIDTH * (i % 3 + 1) / 4
-            y = self.SCREEN_HEIGHT * (i // 3 + 1) / 4
-            r = min(self.SCREEN_HEIGHT, self.SCREEN_WIDTH) * 0.1
+    def draw_circles(self):
+        """draw mini circles"""
+        for circle in self.circles:
             pygame.draw.circle(
-                surface=self.screen, color=(181, 142, 53), center=(x, y), radius=r
-            )
-            """draw mini circles"""
-            color_idx = self.colors[i]
-            circles.append(
-                Circle(center=(x, y), radius=r, color=Game.idx2rgb[color_idx])
+                surface=self.screen,
+                color=Game.circle_color,
+                center=(circle.x, circle.y),
+                radius=circle.radius,
             )
 
-        if not clicked:
-            for c in circles:
+        if not self.clicked:
+            # draw lines over the circles
+            for c in self.circles:
                 diameter = 2 * c.radius
                 y = Game.rgb2idx[c.color] * self.line_ht
                 for _ in range(int(diameter / (3 * self.line_ht))):
                     line = pygame.Surface(size=(diameter, self.line_ht))
                     line.fill(c.color)
-                    self.screen.blit(
-                        line, (c.center[0] - c.radius, c.center[1] - c.radius + y)
-                    )
+                    self.screen.blit(line, (c.x - c.radius, c.y - c.radius + y))
                     y += 3 * self.line_ht
+
+    def tick(self) -> bool:
+        running: bool = True
+        for event in pygame.event.get():  # user input
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.clicked = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.clicked = False
+
+        self.draw_background()
+
+        self.draw_circles()
+
+        return running
 
 
 def main() -> None:
@@ -72,23 +95,12 @@ def main() -> None:
     SCREEN_HEIGHT = 600
 
     running: bool = True
-    clicked = False
 
     game = Game(SCREEN_HEIGHT, SCREEN_WIDTH)
 
     while running:
 
-        for event in pygame.event.get():  # user input
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                clicked = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                clicked = False
-
-        game.draw_background()
-
-        game.draw_circles(clicked)
+        running = game.tick()
 
         pygame.display.flip()  # update display content
 
