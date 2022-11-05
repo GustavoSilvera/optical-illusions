97f8d8c7a4d6e8e0a7a2512bcfee36ff231fd205
Gustavo Silvera <grs5382@gmail.com>
2022-11-04 22:12:25 -0400

    creating a Game class

--- illusion.py
+++ illusion.py
@@ -35,19 +35,69 @@ def col_to_idx(color: tuple) -> int:
     return idx
 
 
+class Game:
+    def __init__(self, h, w, line_ht=2):
+        self.SCREEN_HEIGHT = h
+        self.SCREEN_WIDTH = w
+
+        self.init_game()
+        self.line_ht = line_ht
+
+    def init_game(self):
+        self.screen = pygame.display.set_mode([self.SCREEN_WIDTH, self.SCREEN_HEIGHT])
+        self.line_ht = 2
+        self.colors = [random.randint(a=0, b=2) for _ in range(9)]
+
+    def draw_background(self):
+        self.screen.fill((0, 0, 0))  # background
+        color_idx = 0
+        y = 0
+        for _ in range(self.SCREEN_HEIGHT // self.line_ht):
+            line = pygame.Surface(size=(self.SCREEN_WIDTH, self.line_ht))
+            line.fill(RGBColor(color_idx))
+            color_idx = (color_idx + 1) % 3
+            self.screen.blit(line, (0, y))
+            y += self.line_ht
+
+    def draw_circles(self, clicked: bool):
+        circles = []
+        for i in range(9):
+            x = self.SCREEN_WIDTH * (i % 3 + 1) / 4
+            y = self.SCREEN_HEIGHT * (i // 3 + 1) / 4
+            r = min(self.SCREEN_HEIGHT, self.SCREEN_WIDTH) * 0.1
+            pygame.draw.circle(
+                surface=self.screen, color=(181, 142, 53), center=(x, y), radius=r
+            )
+            """draw mini circles"""
+            color_idx = self.colors[i]
+            circles.append(Circle(center=(x, y), radius=r, color=RGBColor(color_idx)))
+
+        if not clicked:
+            for c in circles:
+                diameter = 2 * c.radius
+                y = col_to_idx(c.color) * self.line_ht
+                for _ in range(int(diameter / (3 * self.line_ht))):
+                    line = pygame.Surface(size=(diameter, self.line_ht))
+                    line.fill(c.color)
+                    self.screen.blit(
+                        line, (c.center[0] - c.radius, c.center[1] - c.radius + y)
+                    )
+                    y += 3 * self.line_ht
+
+
 def main() -> None:
     pygame.init()
 
     # set resolution
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
     running: bool = True
     clicked = False
-    while running:
 
-        random.seed(1)
+    game = Game(SCREEN_HEIGHT, SCREEN_WIDTH)
+
+    while running:
 
         for event in pygame.event.get():  # user input
             if event.type == pygame.QUIT:
@@ -55,45 +105,12 @@ def main() -> None:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 clicked = True
 
-        screen.fill((0, 0, 0))  # background
-        line_ht = 2
-        color_idx = 0
-        y = 0
-        for _ in range(SCREEN_HEIGHT // line_ht):
-            line = pygame.Surface(size=(SCREEN_WIDTH, line_ht))
-            line.fill(RGBColor(color_idx))
-            color_idx = (color_idx + 1) % 3
-            screen.blit(line, (0, y))
-            y += line_ht
+        game.draw_background()
 
-        # draw central circles
-        circles = []
-        for i in range(9):
-            x = SCREEN_WIDTH * (i % 3 + 1) / 4
-            y = SCREEN_HEIGHT * (i // 3 + 1) / 4
-            r = min(SCREEN_HEIGHT, SCREEN_WIDTH) * 0.1
-            pygame.draw.circle(
-                surface=screen, color=(181, 142, 53), center=(x, y), radius=r
-            )
-            """draw mini circles"""
-            color_idx = random.randint(a=0, b=2)  # R, G, B
-            circles.append(Circle(center=(x, y), radius=r, color=RGBColor(color_idx)))
-
-        if not clicked:
-            for c in circles:
-                diameter = 2 * c.radius
-                y = col_to_idx(c.color) * line_ht
-                for _ in range(int(diameter / (3 * line_ht))):
-                    line = pygame.Surface(size=(diameter, line_ht))
-                    line.fill(c.color)
-                    screen.blit(
-                        line, (c.center[0] - c.radius, c.center[1] - c.radius + y)
-                    )
-                    y += 3 * line_ht
+        game.draw_circles(clicked)
 
         pygame.display.flip()  # update display content
 
-    # Done! Time to quit.
     pygame.quit()
 
 
