75526c15f486b8af5f78040c66112628e8ad9391
Gustavo Silvera <grs5382@gmail.com>
2022-11-05 09:54:20 -0400

    enabling wall collisions and resizeable window

--- illusion.py
+++ illusion.py
@@ -9,11 +9,18 @@ class Circle:
         self.y = y
         self.color = color
         self.radius = radius
-        self.vel_x, self.vel_y = 0.01, 0.02
+        self.vel_x = random.random() - 0.5  # random [-0.5, 0.5]
+        self.vel_y = random.random() - 0.5  # random [-0.5, 0.5]
 
-    def tick(self, dt):
+    def tick(self, dt, ht, wt):
         self.x += dt * self.vel_x
         self.y += dt * self.vel_y
+        if self.x > wt - self.radius or self.x < self.radius:
+            self.vel_x *= -1
+        if self.y > ht - self.radius or self.y < self.radius:
+            self.vel_y *= -1
+        self.x = max(min(self.x, wt - self.radius), self.radius)
+        self.y = max(min(self.y, ht - self.radius), self.radius)
 
 
 class Game:
@@ -26,18 +33,21 @@ class Game:
         self.SCREEN_WIDTH = w
 
         self.line_ht = line_ht
-        self.screen = pygame.display.set_mode([self.SCREEN_WIDTH, self.SCREEN_HEIGHT])
+        self.screen = pygame.display.set_mode(
+            [self.SCREEN_WIDTH, self.SCREEN_HEIGHT], pygame.RESIZABLE, vsync=60
+        )
+        pygame.display.set_caption("gsilvera's E/A/C Illusion game")
         self.clock = pygame.time.Clock()
 
         self.line_ht = 2
         self.circles = [
             Circle(
-                x=self.SCREEN_WIDTH * (i % 3 + 1) / 4,
-                y=self.SCREEN_HEIGHT * (i // 3 + 1) / 4,
+                x=self.SCREEN_WIDTH * ((i % 9) % 3 + 1) / 4,
+                y=self.SCREEN_HEIGHT * ((i % 9) // 3 + 1) / 4,
                 radius=min(self.SCREEN_HEIGHT, self.SCREEN_WIDTH) * 0.1,
                 color=Game.idx2rgb[random.randint(a=0, b=2)],
             )
-            for i in range(9)
+            for i in range(100)
         ]
 
         # inputs
@@ -63,7 +73,7 @@ class Game:
                 center=(circle.x, circle.y),
                 radius=circle.radius,
             )
-            circle.tick(dt)
+            circle.tick(dt, self.SCREEN_HEIGHT, self.SCREEN_WIDTH)
 
         if not self.clicked:
             # draw lines over the circles
@@ -93,6 +103,12 @@ class Game:
                 self.clicked = True
             elif event.type == pygame.MOUSEBUTTONUP:
                 self.clicked = False
+            if event.type == pygame.VIDEORESIZE:
+                self.SCREEN_HEIGHT = event.h
+                self.SCREEN_WIDTH = event.w
+                self.screen = pygame.display.set_mode(
+                    (self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.RESIZABLE
+                )
 
         self.draw_background()
 
